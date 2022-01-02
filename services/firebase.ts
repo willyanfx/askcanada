f4004b6c1c9fc04ca3203553d644058a8c6e91bc
willyanfx <willyan.fx@gmail.com>
2022-01-02 16:16:46 -0300

    firebase config/init

---
+++ services/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'process.env.REACT_APP_API_KEY',
+  authDomain: 'process.env.REACT_APP_AUTH_DOMAIN',
+  projectId: 'process.env.REACT_APP_PROJ_ID',
+  storageBucket: 'process.env.REACT_APP_STORAGE',
+  messagingSenderId: 'process.env.REACT_APP_SENDER',
+  appId: 'process.env.REACT_APP_APP_ID',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+const auth = getAuth()
+const database = getFirestore()
+
+export { firebaseApp, auth, database }
