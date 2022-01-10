6cc0e63ee2a7220ae06fc74705b64b287a42df2c
willyanfx <willyan.fx@gmail.com>
2022-01-09 21:47:02 -0300

    start w tailwind

--- components/button.tsx
+++ components/button.tsx
@@ -14,10 +14,26 @@ const Button = ({
   isDisabled = false,
   onClick,
   className,
+  fullWidth,
+  variant = 'primary',
   ...props
 }: ButtonProps): JSX.Element => {
+  const cx = [
+    variant === 'outline' && 'border border-gray-500',
+    variant === 'primary' &&
+      'bg-red-600 text-white hover:bg-red-700 hover:text-gray-200',
+    fullWidth && 'w-full',
+  ]
+    .filter(Boolean)
+    .join(' ')
+
   return (
-    <button {...props} onClick={onClick} disabled={isDisabled}>
+    <button
+      className={`${cx} text-sm font-semibold  py-3 px-4 rounded-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-red-700 focus-visible:ring-offset-2 focus-visible:ring-offset-white flex justify-center leading-none`}
+      {...props}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
       {children}
     </button>
   )
